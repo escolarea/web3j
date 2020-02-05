8a33541791e25db84e5d9457d029f83a73c41022
Wilmer Carranza <wcarranzam91@gmail.com>
2020-02-04 18:12:17 -0600

    Replaces Optional in WebSocketClient

--- core/src/main/java/org/web3j/protocol/websocket/WebSocketClient.java
+++ core/src/main/java/org/web3j/protocol/websocket/WebSocketClient.java
@@ -2,7 +2,6 @@ package org.web3j.protocol.websocket;
 
 import java.net.URI;
 import java.util.Map;
-import java.util.Optional;
 
 import org.java_websocket.handshake.ServerHandshake;
 import org.slf4j.Logger;
@@ -16,7 +15,7 @@ public class WebSocketClient extends org.java_websocket.client.WebSocketClient {
 
     private static final Logger log = LoggerFactory.getLogger(WebSocketClient.class);
 
-    private Optional<WebSocketListener> listenerOpt = Optional.empty();
+    private WebSocketListener listenerOpt = null;
 
     public WebSocketClient(URI serverUri) {
         super(serverUri);
@@ -34,26 +33,30 @@ public class WebSocketClient extends org.java_websocket.client.WebSocketClient {
     @Override
     public void onMessage(String s) {
         log.debug("Received message {} from server {}", s, uri);
-        listenerOpt.ifPresent(listener -> {
+        if (listenerOpt != null) {
             try {
-                listener.onMessage(s);
+                listenerOpt.onMessage(s);
             } catch (Exception e) {
                 log.error("Failed to process message '{}' from server {}", s, uri, e);
             }
-        });
+        }
     }
 
     @Override
     public void onClose(int code, String reason, boolean remote) {
         log.info("Closed WebSocket connection to {}, because of reason: '{}'."
                 + "Connection closed remotely: {}", uri, reason, remote);
-        listenerOpt.ifPresent(WebSocketListener::onClose);
+        if (listenerOpt != null) {
+            listenerOpt.onClose();
+        }
     }
 
     @Override
     public void onError(Exception e) {
         log.error("WebSocket connection to {} failed with error", uri, e);
-        listenerOpt.ifPresent(listener -> listener.onError(e));
+        if (listenerOpt != null) {
+            listenerOpt.onError(e);
+        }
     }
 
     /**
@@ -62,6 +65,6 @@ public class WebSocketClient extends org.java_websocket.client.WebSocketClient {
      * @param listener WebSocket listener
      */
     public void setListener(WebSocketListener listener) {
-        this.listenerOpt = Optional.ofNullable(listener);
+        this.listenerOpt = listener;
     }
 }
